75bf0efc463345507a906cf8ac50512a2a7449e0
denisdpc <denisdpc@gmail.com>
2023-06-19 15:54:50 -0300

    texto dominio tecnologico

--- app/src/lib/DominioTecnologico.svelte
+++ app/src/lib/DominioTecnologico.svelte
@@ -2,9 +2,9 @@
     import Opcionario from '$lib/Opcionario.svelte';
     import { dominioTecnologico } from '$lib/shared/stores.js';
     const lista = [
-        {id: 3, item: 'Avançado', descricao: ''},
-        {id: 2, item: 'Intermediário', descricao: ''},
-        {id: 1, item: 'Básico', descricao: ''},
+        {id: 3, item: 'Avançado', descricao: 'pesquisa & desenvolvimento autônomos ou em conjunto com centros de pesquisa e universidades nacionais para viabilizar a sua concepção'},
+        {id: 2, item: 'Intermediário', descricao: 'Engenharia terceirizada nacional, estabelecimento de consórcios e alianças estratégicas, de parte ou total, para viabilizar a sua concepção'},
+        {id: 1, item: 'Básico', descricao: 'dependência operativa ou tecnológica internacional, de parte ou total, para viabilizar a sua concepção'},
     ]        
 </script>
 
