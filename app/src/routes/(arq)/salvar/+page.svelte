e6f82e00898ae79a52c39e807d7bf8893667adf7
denisdpc <denisdpc@gmail.com>
2023-06-21 15:54:16 -0300

    arq json

--- app/src/routes/(arq)/salvar/+page.svelte
+++ app/src/routes/(arq)/salvar/+page.svelte
@@ -35,7 +35,7 @@
         const a = document.createElement("a");
         const file = new Blob([content], {type: "text/plain"});
         a.href = URL.createObjectURL(file);
-        a.download = fileName;
+        a.download = fileName.concat('.json');
         a.click();
     }
 
