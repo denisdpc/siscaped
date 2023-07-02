94ab4818c2642394abfbdf35ce9154a20ba81c53
denisdpc <denisdpc@gmail.com>
2023-07-02 15:41:40 -0300

    pdf table

--- app/src/routes/relatorio/+page.svelte
+++ app/src/routes/relatorio/+page.svelte
@@ -1,25 +1,78 @@
+<!-- https://github.com/simonbengtsson/jsPDF-AutoTable
+https://github.com/simonbengtsson/jsPDF-AutoTable/blob/master/examples/typescript/index.ts -->
+
 <script>
-    function imprimir() {
-        console.log('imprimir')
+    import { jsPDF } from 'jspdf'
+    import autoTable from 'jspdf-autotable'
 
-        const tabela = document.getElementById('tabela')?.innerHTML;
-        const estilo = document.getElementById('estilo')?.innerHTML;
+    function gerarPDF() {
+        const doc = new jsPDF()
+        doc.text("hello word",12,12)
+        autoTable(doc, {
+            theme: 'grid',
+            head: [['ID', 'Country', 'Index', 'Capital']],
+            body: [
+                [1, 'Finland', 7.632, 'Helsinki'],
+                [2, 'Norway', 7.594, 'Oslo'],
+                [3, 'Denmark', 7.555, 'Copenhagen'],
+                [4, 'Iceland', 7.495, 'Reykjavík'],
+                [5, 'Switzerland', 7.487, 'Bern'],
+                [9, 'Sweden', 7.314, 'Stockholm'],
+                [73, 'Belarus', 5.483, 'Minsk'],
+                [{ content: 'Text', colSpan: 2, rowSpan: 2, styles: { halign: 'center' } }],                
+            ],
+        })
+        autoTable(doc, {
+            theme: 'grid',
+            head: [['ID', 'Country', 'Index', 'Capital']],
+            body: [
+                [1, 'Finland', 7.632, 'Helsinki'],
+                [2, 'Norway', 7.594, 'Oslo'],
+                [3, 'Denmark', 7.555, 'Copenhagen'],
+                [4, 'Iceland', 7.495, 'Reykjavík'],
+                [5, 'Switzerland', 7.487, 'Bern'],
+                [9, 'Sweden', 7.314, 'Stockholm'],
+                [73, 'Belarus', 5.483, 'Minsk'],
+                [{ content: 'Text', colSpan: 2, rowSpan: 2, styles: { halign: 'center' } }],                
+            ],
+        })
 
-        //console.log(tabela);
-        //console.log(estilo);
+        const tabelaElemento = document.getElementById('tabela')
+        //doc.fromHTML(tabelaElemento);
 
-        const win = window.open('','','height=700,width=700');
-        win?.document.write('<html><head>')
-        win?.document.write('<title>SisCaPed</title>')
-        win?.document.write(estilo);   
-        win?.document.write('</head>')
-        win?.document.write('<body>')
-        win?.document.write(tabela);
-        win?.document.write('</body></html>')
-        win?.print()        
+        doc.save('table.pdf')
     }
+
+    function HTMLtoPDF() {          
+        let doc = new jsPDF('l', 'mm', [1500, 1400]);
+        //let pdfjs = document.querySelector('#tabela');
+        let pdfjs = document.getElementById('tabela')
+        console.log(pdfjs)
+
+        doc.html(pdfjs, {
+            callback: function(doc) {
+                doc.save("newpdf.pdf");
+            },
+            x: 12,
+            y: 12
+        });         
+    }
+    
 </script>
 
+relatorio
+<br>
+<button on:click={ gerarPDF } 
+        type="button" 
+        class="btn variant-filled">Gerar PDF</button>
+
+<button on:click={ HTMLtoPDF } 
+        type="button" 
+        class="btn variant-filled">HTML to PDF</button>
+
+    
+<br>
+<br>
 <table id='tabela'>
     <tbody>
         <tr>
@@ -31,11 +84,6 @@
         <tr><td>Maturidade Tecnológica</td><td>6</td></tr>
     </tbody>    
 </table>
-
-<br>
-<button on:click={imprimir} 
-        type="button" 
-        class="btn variant-filled">Imprimir</button>
 
 <div id='estilo'>
     <style>
--- app/src/routes/relatorio/old_page.svelte
+++ app/src/routes/relatorio/old_page.svelte
@@ -1,34 +1,24 @@
-<!-- https://www.youtube.com/watch?v=z8CSuoFX7Ko -->
 <script>
     function imprimir() {
         console.log('imprimir')
-        // const tabela = document.getElementById('tabela')?.innerHTML;
-        // const estilo = document.getElementById('estilo')?.innerHTML;
 
-        // let estilo = '<style>
-        // estilo += 'table, th, td {'
-        // estilo += 'border: 1px solid;'
-        // estilo += 'border-collapse: collapse;'
-        // estilo += 'margin-bottom: 10px;'
-        // estilo += '</style>'
+        const tabela = document.getElementById('tabela')?.innerHTML;
+        const estilo = document.getElementById('estilo')?.innerHTML;
 
-        // const win = window.open('','','height=700,width=700');
-        // win?.document.write('<html><head>')
-        // win?.document.write('<title>SisCaPed</title>')
-        // win?.document.write(estilo);
-        // win?.document.write('</head>')
-        // win?.document.write('<body>')        
-        // win?.document.write(tabela);
-        // win?.document.write('</body></html>')
+        //console.log(tabela);
+        //console.log(estilo);
 
-        // win?.print()
-        
+        const win = window.open('','','height=700,width=700');
+        win?.document.write('<html><head>')
+        win?.document.write('<title>SisCaPed</title>')
+        win?.document.write(estilo);   
+        win?.document.write('</head>')
+        win?.document.write('<body>')
+        win?.document.write(tabela);
+        win?.document.write('</body></html>')
+        win?.print()        
     }
 </script>
-
-<br>
-<button on:click={imprimir} type="button" class="btn variant-filled">Imprimir</button>
-<br><br>
 
 <table id='tabela'>
     <tbody>
@@ -42,11 +32,17 @@
     </tbody>    
 </table>
 
-<style>
-    /* table, th, td {
-		border: 1px solid;
-		border-collapse: collapse;
-		margin-bottom: 10px;
-	}; */
-</style>
+<br>
+<button on:click={imprimir} 
+        type="button" 
+        class="btn variant-filled">Imprimir</button>
 
+<div id='estilo'>
+    <style>
+        table, th, td {
+            border: 1px solid;
+            border-collapse: collapse;
+            margin-bottom: 10px;
+        }
+    </style>
+</div>
